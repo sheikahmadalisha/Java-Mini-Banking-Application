93d5ce3ceb7e175cea7ba4f184c6f514a79c57da
sheikahmadalisha <152063875+sheikahmadalisha@users.noreply.github.com>
2025-11-03 19:32:21 +0530

    final commit

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -8,7 +8,7 @@ public class DatabaseConnection {
 	public static Connection getConnection() {
 		String url = "jdbc:mysql://localhost:3306/gkbankdb";
 		String user = "root";
-		String pass = "123456789";
+		String pass = "1234567890";
 		Connection con = null;
 		
 		try { 
