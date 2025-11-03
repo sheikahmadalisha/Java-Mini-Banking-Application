6961f905e9fcfe16d3687a95b7dcb0fd4cfbfd1a
sheikahmadalisha <152063875+sheikahmadalisha@users.noreply.github.com>
2025-11-03 19:18:15 +0530

    Create DatabaseConnection.java

---
+++ DatabaseConnection.java
@@ -0,0 +1,25 @@
+package com.learn.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConnection {
+	
+	public static Connection getConnection() {
+		String url = "jdbc:mysql://localhost:3306/gkbankdb";
+		String user = "root";
+		String pass = "123456789";
+		Connection con = null;
+		
+		try { 
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection(url, user, pass);
+			System.out.println("✅ Connected to database!");
+			
+		} catch(Exception e) {
+			System.out.println("❌ Exception occurred while connecting to database");
+			e.printStackTrace();
+		}
+		return con;
+	}
+}
